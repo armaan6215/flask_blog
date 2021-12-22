eb2b53a67032cfc514943900f7c9101bed0f54a3
Manish Maharjan <manish@192-168-1-103.tpgi.com.au>
2021-12-23 00:03:18 +1100

    Populating database from backend with python code

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import sqlite3 as sql
 from flask import Flask, render_template
 app = Flask(__name__)
+
+def db_connection():
+    conn = sql.connect('database.db')
+    conn.row_factory = sql.Row
+    return conn
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/posts")
+def posts():
+    query = "SELECT * FROM POSTS"
+    conn = db_connection()
+    posts = conn.execute(query).fetchall()
+    conn.close()
+    return render_template("posts.html", posts = posts)
 
 app.run(debug=True)
