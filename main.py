a5c2a4716a57e05bf5eae479ea88e0e714461a4c
Manish Maharjan <manish@192-168-1-103.tpgi.com.au>
2021-12-23 02:01:52 +1100

    Added timestamp for posts

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import re
 import sqlite3 as sql
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "96159"
 
 
 def db_connection():
@@ -18,7 +19,7 @@ def index():
 
 @app.route("/create", methods=("GET", "POST"))
 def create_post():
-    if (request.method == "POST"):
+    if request.method == "POST":
         title = request.form["title"]
         content = request.form["content"]
         author = request.form["author"]
@@ -30,7 +31,7 @@ def create_post():
         )
         conn.commit()
         conn.close()
-        return redirect(url_for('posts'))
+        return redirect(url_for("posts"))
 
     return render_template("create_post.html")
 
@@ -44,4 +45,4 @@ def posts():
     return render_template("posts.html", posts=posts)
 
 
-app.run(debug=True)
+app.run(host="192.168.1.103", port=5000, threaded=True, debug=True)
